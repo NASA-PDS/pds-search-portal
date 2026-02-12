ee62927712211212b4ca2567e682434836b15537
Eddie Arevalo <eddiesarevalo@gmail.com>
2026-02-12 13:48:19 -0800

    Test 5

--- src/ts/pages/DataCatalog/DataCatalog.tsx
+++ src/ts/pages/DataCatalog/DataCatalog.tsx
@@ -313,7 +313,7 @@ const DataCatalog: React.FC = () => {
               <>
                 <header className="hzn-main-header">
                   <Container className="pt-4">
-                    <h1>Data Catalog Github Action Test 4</h1>
+                    <h1>Data Catalog Github Action Test 5</h1>
                     <Form onSubmit={formHandleSubmit}>
                       <div className="hzn-search hzn-global-header">
                         <Form.Control
