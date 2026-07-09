b1a8e0459cf77a7aa444a7197e15162db2240175
Eddie Arevalo <eddiesarevalo@gmail.com>
2026-07-09 10:35:36 -0700

    Restore Title
    -Restored the title with minor css correction.

--- src/ts/pages/DataCatalog/DataCatalog.tsx
+++ src/ts/pages/DataCatalog/DataCatalog.tsx
@@ -313,6 +313,7 @@ const DataCatalog: React.FC = () => {
               <>
                 <header className="hzn-main-header">
                   <Container className="pt-4">
+                    <h1>PDS Search</h1>
                     <Form onSubmit={formHandleSubmit}>
                       <div className="hzn-search hzn-global-header">
                         <Form.Control
